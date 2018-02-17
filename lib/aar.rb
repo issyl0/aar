985ec71d4ed57dc21f7c8be7dec2fce195744cb0
Isabell (Issy) Long <isabell@issyl0.co.uk>
2018-02-17 17:40:33 +0000

    Separate the optparse code for CLI options into its own file

--- lib/aar.rb
+++ lib/aar.rb
@@ -2,29 +2,12 @@ require 'yaml'
 require 'optparse'
 
 require_relative 'aar/version'
+require_relative 'aar/cli'
+
+Aar::Cli
 
 module Aar
   class AssumeRole
-    def initialize(args)
-      args << '-h' if ARGV.empty?
-        options = {}
-        OptionParser.new do |opts|
-          opts.banner = "Usage: bundle exec aar -e environment -o output -m mfa_code"
-
-          opts.on('-e ENVIRONMENT', '--environment=ENVIRONMENT', 'The environment you\'re assuming the role for.') { |v| options[:environment] = v }
-          opts.on('-o OUTPUT', '--output=OUTPUT', 'How to output the tokens: print them to stdout, or export them into the shell.') { |v| options[:output] = v }
-          opts.on('-m CODE', '--mfa-code=CODE', OptionParser::DecimalInteger, 'The MFA code of your user account.') { |v| options[:mfa_code] = v }
-
-          opts.on_tail("-h", "--help", "Show this help message.") do
-            puts opts
-            exit
-          end
-        end.parse!
-
-        @output = options[:output]
-        @environment = options[:environment]
-        @mfa_code = options[:mfa_code]
-    end
 
     def call
       config
