578dceefc0d9605a6c3586a54f4471d077af65b1
Isabell (Issy) Long <isabell@issyl0.co.uk>
2018-02-17 17:53:21 +0000

    Remove the old layout and call method from aar.rb

--- lib/aar.rb
+++ lib/aar.rb
@@ -14,20 +14,3 @@ Aar::Config
 Aar::Commands.new
 Aar::AccessKeys.new
 Aar::TokenExpiry.print_token_expiry
-
-module Aar
-  class AssumeRole
-
-    def call
-      config
-      construct_command
-      run_command
-      if @output == "export" || @output.nil?
-        export_access_keys
-      elsif @output == "print"
-        print_access_keys
-      end
-      print_token_expiry
-    end
-  end
-end
